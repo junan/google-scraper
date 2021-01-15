81d2d6e784a226e29811816082513273eeeb3d7f
Junan Chakma <junan@nimblehq.co>
2021-01-15 18:17:07 +0700

    Removed password confirmation field

--- models/user.go
+++ models/user.go
@@ -1,16 +1,11 @@
 package models
 
-import (
-	"time"
-)
-
 type User struct {
-	Id                int
+	Base
+
 	Name              string
 	Email             string `orm:"unique"`
 	EncryptedPassword string
-	CreatedAt         time.Time `orm:"auto_now_add;type(datetime)"`
-	UpdatedAt         time.Time `orm:"auto_now;type(datetime)"`
 }
 
 // Beego by default creates the table name as singular, it will make it plural
