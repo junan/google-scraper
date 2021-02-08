d66a907aecdc7b517f746052cdc8c4b929bb107b
Junan Chakma <junan@nimblehq.co>
2021-02-08 16:37:10 +0700

    Use logs.Critical

--- db/db.go
+++ db/db.go
@@ -1,11 +1,10 @@
 package db
 
 import (
-	"fmt"
-
 	"google-scraper/models"
 
 	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/core/logs"
 	"github.com/beego/beego/v2/server/web"
 	_ "github.com/lib/pq"
 )
@@ -13,19 +12,19 @@ import (
 func init() {
 	dbUrl, err := web.AppConfig.String("dbUrl")
 	if err != nil {
-		fmt.Println("Postgres database source is not found: ", err)
+		logs.Critical("Postgres database source is not found: ", err)
 	}
 
 	// Register postgres driver
 	err = orm.RegisterDriver("postgres", orm.DRPostgres)
 	if err != nil {
-		fmt.Println("Postgres driver registration failed: ", err)
+		logs.Critical("Postgres driver registration failed: ", err)
 	}
 
 	// Register the database
 	err = orm.RegisterDataBase("default", "postgres", dbUrl)
 	if err != nil {
-		fmt.Println("Postgres connection failed: ", err)
+		logs.Critical("Postgres connection failed: ", err)
 	}
 
 	// Register models
@@ -34,7 +33,7 @@ func init() {
 	// Keeping the models and database in sync
 	err = orm.RunSyncdb("default", false, true)
 	if err != nil {
-		fmt.Println("Failed to sync models with the database:", err)
+		logs.Critical("Failed to sync models with the database:", err)
 	}
 
 	mode, _ := web.AppConfig.String("runmode")
