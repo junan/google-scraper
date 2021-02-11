e8c050222679750bda2751403a123b3c9e6941e2
Junan Chakma <junan@nimblehq.co>
2021-02-11 15:52:33 +0700

    Use the error class

--- controllers/base.go
+++ controllers/base.go
@@ -16,9 +16,6 @@ type baseController struct {
 func (c *baseController) Prepare() {
 	helpers.SetDataAttributes(&c.Controller)
 
-	c.LayoutSections = make(map[string]string)
-	c.LayoutSections["FlashMessage"] = "shared/alert.html"
-
 	app, ok := c.AppController.(NestPreparer)
 	if ok {
 		app.NestPrepare()
