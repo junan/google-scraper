2fb64d41d1be8b53eef539816e82fed5ece19c03
Junan Chakma <junan@nimblehq.co>
2021-02-16 18:38:51 +0700

    Added session test spec

---
+++ controllers/session_test.go
@@ -0,0 +1,102 @@
+package controllers_test
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+
+	_ "google-scraper/initializers"
+	. "google-scraper/tests/fabricators"
+	. "google-scraper/tests/testing_helpers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SessionController", func() {
+	Describe("GET /login", func() {
+		Context("given the user is a guest user", func() {
+			It("returns 200 status code", func() {
+				response := MakeRequest("GET", "/login", nil)
+
+				Expect(response.StatusCode).To(Equal(http.StatusOK))
+			})
+		})
+	})
+
+	Describe("POST /login", func() {
+		Context("given the session params are valid", func() {
+			It("redirects to the root path", func() {
+				email := "john@example.com"
+				password := "secret"
+				FabricateUser("John", email, password)
+				params := url.Values{
+					"email":    {email},
+					"password": {password},
+				}
+				body := strings.NewReader(params.Encode())
+
+				response := MakeRequest("POST", "/login", body)
+				path := GetUrlPath(response)
+
+				Expect(response.StatusCode).To(Equal(http.StatusFound))
+				Expect(path).To(Equal("/"))
+			})
+
+			It("sets the flash success message", func() {
+				email := "john@example.com"
+				password := "secret"
+				FabricateUser("John", email, password)
+				params := url.Values{
+					"email":    {email},
+					"password": {password},
+				}
+
+				body := strings.NewReader(params.Encode())
+				response := MakeRequest("POST", "/login", body)
+				flash := GetFlash(response.Cookies())
+
+				Expect(flash.Data["success"]).To(Equal("Signed in successfully."))
+			})
+		})
+
+		Context("given the session params are INVALID", func() {
+			It("redirects to the login path", func() {
+				email := "invalid-email"
+				password := "secret"
+				FabricateUser("John", email, password)
+				params := url.Values{
+					"email":    {email},
+					"password": {password},
+				}
+				body := strings.NewReader(params.Encode())
+
+				response := MakeRequest("POST", "/login", body)
+				path := GetUrlPath(response)
+
+				Expect(response.StatusCode).To(Equal(http.StatusFound))
+				Expect(path).To(Equal("/login"))
+			})
+
+			It("sets the flash error message", func() {
+				email := "invalid-email"
+				password := "secret"
+				FabricateUser("John", email, password)
+				params := url.Values{
+					"email":    {email},
+					"password": {password},
+				}
+				body := strings.NewReader(params.Encode())
+
+				response := MakeRequest("POST", "/login", body)
+				flash := GetFlash(response.Cookies())
+
+				Expect(flash.Data["error"]).To(Equal("Email must be a valid email address."))
+			})
+		})
+	})
+
+	AfterEach(func() {
+		TruncateTable("users")
+	})
+})
--- tests/testing_helpers/http.go
+++ tests/testing_helpers/http.go
@@ -1,15 +1,17 @@
 package testing_helpers
 
 import (
+	"context"
+	"github.com/beego/beego/v2/server/web"
+	"github.com/onsi/ginkgo"
+	"google-scraper/models"
+	"google-scraper/controllers"
 	"io"
 	"net/http"
 	"net/http/httptest"
-
-	"github.com/beego/beego/v2/server/web"
-	"github.com/onsi/ginkgo"
 )
 
-func MakeRequest(method string, url string, body io.Reader)  *http.Response {
+func MakeRequest(method string, url string, body io.Reader) *http.Response {
 	request, err := http.NewRequest(method, url, body)
 
 	if err != nil {
@@ -21,4 +23,27 @@ func MakeRequest(method string, url string, body io.Reader)  *http.Response {
 	web.BeeApp.Handlers.ServeHTTP(response, request)
 
 	return response.Result()
+}
+
+func MakeAuthenticatedRequest(method string, url string, body io.Reader, user *models.User) *http.Response {
+	request, err := http.NewRequest(method, url, body)
+	if err != nil {
+		ginkgo.Fail("Creating request failed: " + err.Error())
+	}
+
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	responseRecorder := httptest.NewRecorder()
+	store, err := web.GlobalSessions.SessionStart(responseRecorder, request)
+	if err != nil {
+		ginkgo.Fail("Starting session failed: " + err.Error())
+	}
+
+	err = store.Set(context.Background(), controllers.CurrentUserSession, user.Id)
+	if err != nil {
+		ginkgo.Fail("Setting current user failed:  " + err.Error())
+	}
+
+	web.BeeApp.Handlers.ServeHTTP(responseRecorder, request)
+
+	return responseRecorder.Result()
 }
