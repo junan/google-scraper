4132290e3b66006051c700ed9ff4aad6bf44e943
Junan Chakma <junan@nimblehq.co>
2021-02-22 17:19:10 +0700

    Made two navbar partial

--- helpers/controller.go
+++ helpers/controller.go
@@ -13,19 +13,8 @@ func SetDataAttributes(c *web.Controller, user *models.User) {
 	c.Data["ControllerName"] = strcase.ToSnake(controllerName)
 	c.Data["ActionName"] = strcase.ToSnake(actionName)
 	c.Data["CurrentUser"] = user
-	c.Data["navbarExpandModifierCssClass"] = navbarExpandModifierCssClass(user)
 	c.Data["AlertMap"] = map[string]string{
 		"success": "success",
 		"error":   "danger",
 	}
-}
-
-func navbarExpandModifierCssClass(user *models.User) string {
-	modifier := ""
-
-	if user != nil {
-		modifier = "-lg"
-	}
-
-	return modifier
 }
