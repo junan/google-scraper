d96d529b6b8a5c224d2f56aa0e7c0b46ead78d63
Junan Chakma <junan@nimblehq.co>
2021-02-25 15:40:14 +0700

    Added vcr functionality

---
+++ tests/testing_helpers/vcr.go
@@ -0,0 +1,25 @@
+package testing_helpers
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/dnaeon/go-vcr/recorder"
+	. "github.com/onsi/ginkgo"
+)
+
+func RecordCassette(cassetteName string, searchString string) {
+	searchUrl := fmt.Sprintf("https://www.google.com/search/?q=%s&lr=lang_en&hl=en", url.QueryEscape(searchString))
+	recorder, err := recorder.New(fmt.Sprintf("%s/tests/fixtures/vcr/%s", AppRootDir(), cassetteName))
+	if err != nil {
+		Fail(err.Error())
+	}
+	defer recorder.Stop()
+
+	client := &http.Client{Transport: recorder}
+	_, err = client.Get(searchUrl)
+	if err != nil {
+		Fail(err.Error())
+	}
+}
