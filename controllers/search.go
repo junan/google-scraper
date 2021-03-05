cd33f2ca16f9836fbd266abb32b87ff2d0afb5f1
Junan Chakma <junan@nimblehq.co>
2021-03-05 10:01:11 +0700

    Use separate method for finding size

--- controllers/search.go
+++ controllers/search.go
@@ -1,12 +1,12 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"google-scraper/forms"
 
 	"github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/core/logs"
 )
 
 type Search struct {
@@ -14,19 +14,20 @@ type Search struct {
 }
 
 func (c *Search) Create() {
-	file, header, _ := c.GetFile("file")
 	redirectPath := "/"
 	flash := web.NewFlash()
+	file, header, err := c.GetFile("file")
 
-	errs := forms.SearchProcess(file, header)
+	if err != nil {
+		logs.Error("Getting file failed: ", err)
+	}
+	err = forms.PerformSearch(file, header)
 
-	if len(errs) > 0 {
-		flash.Error(fmt.Sprint(errs))
+	if err != nil {
+		flash.Error(err.Error())
 	} else {
 		flash.Success("You csv file has been uploaded")
 	}
-
 	flash.Store(&c.Controller)
 	c.Ctx.Redirect(http.StatusFound, redirectPath)
 }
-
--- forms/search_form.go
+++ forms/search_form.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"mime/multipart"
 	"path"
+
 	"github.com/beego/beego/v2/core/validation"
 )
 
@@ -15,20 +16,27 @@ var allowedExtensionMap = map[string]bool{
 type CSV struct {
 	File   multipart.File
 	Header *multipart.FileHeader
+	Size   int64
 }
 
 func (csv *CSV) Valid(v *validation.Validation) {
 	// Verifying file is not empty
 	if csv.File == nil {
-		_ = v.SetError("File", "Uploaded file can't be blank")
+		_ = v.SetError("File", "File can't be blank")
+		return
+	}
+
+	// Verifying file size is not more than 5MB
+	if csv.Size > 5 {
+		_ = v.SetError("Size", "File size can't be more than 5 MB")
 		return
 	}
 
 	// Verifying uploaded file is in CSV format
 	extension := path.Ext(csv.Header.Filename)
 	_, ok := allowedExtensionMap[extension]
-	if !ok  {
-		_ = v.SetError("File", "Uploaded file should be in CSV format")
+	if !ok {
+		_ = v.SetError("File", "File should be in CSV format")
 		return
 	}
 
@@ -39,24 +47,24 @@ func (csv *CSV) Valid(v *validation.Validation) {
 	}
 }
 
-func SearchProcess(file multipart.File, header *multipart.FileHeader) (errs []error) {
-	csvFile := CSV{File: file, Header: header}
+func PerformSearch(file multipart.File, header *multipart.FileHeader) (err error) {
+	csvFile := CSV{File: file, Header: header, Size: getSizeInMb(header)}
 	validation := validation.Validation{}
 	success, err := validation.Valid(&csvFile)
 
 	if err != nil {
-		return errs
+		return err
 	}
 
 	if !success {
 		for _, err := range validation.Errors {
-			errs = append(errs, err)
+			return err
 		}
 	}
 
 	// Do the cron jobs
 
-	return errs
+	return err
 }
 
 func readKeywords(file multipart.File) ([][]string, error) {
@@ -74,4 +82,15 @@ func readKeywords(file multipart.File) ([][]string, error) {
 	}
 
 	return keywords, nil
+}
+
+func getSizeInMb(header *multipart.FileHeader) int64 {
+	var size int64
+	if header == nil {
+		size = 0
+	} else {
+		size = header.Size / (1024 * 1024)
+	}
+
+	return size
 }
