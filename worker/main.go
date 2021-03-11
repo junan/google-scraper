9c85e314ec177573ef93a92c11fdc3eba8dda5f1
Junan Chakma <junan@nimblehq.co>
2021-03-11 18:05:54 +0700

    More clean up

--- worker/main.go
+++ worker/main.go
@@ -5,9 +5,10 @@ import (
 	"os/signal"
 	"syscall"
 
-	"google-scraper/database"
 	_ "google-scraper/initializers"
+	"google-scraper/database"
 	"google-scraper/worker/jobs"
+
 
 	"github.com/gocraft/work"
 )
@@ -17,14 +18,16 @@ func init() {
 
 func main() {
 	pool := work.NewWorkerPool(jobs.Context{}, 5, "google_scraper", database.GetRedisPool())
-
 	pool.JobWithOptions("crawling_job", work.JobOptions{MaxFails: jobs.MaxFails}, (*jobs.Context).PerformCrawling)
 
+	// Start processing jobs
 	pool.Start()
 
+	// Wait for a signal to quit:
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 	<-signalChan
 
+	// Stop the pool
 	pool.Stop()
 }
