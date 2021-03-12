cbdfb707f4119644e10d451e1d25b5badbf702e6
Junan Chakma <junan@nimblehq.co>
2021-03-12 17:52:38 +0700

    Added basic query

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"google-scraper/models"
+
+	"github.com/beego/beego/v2/core/logs"
 	"github.com/beego/beego/v2/server/web"
 )
 
@@ -12,6 +15,14 @@ func (c *Dashboard) New() {
 	web.ReadFromRequest(&c.Controller)
 
 	c.setAttributes()
+
+
+	keywords, err := models.FetchKeywords(c.CurrentUser)
+	if err != nil {
+		logs.Error("Fetching keywords failed: ", err)
+	}
+
+	c.Data["Keywords"] = keywords
 }
 
 func (c *Dashboard) setAttributes() {
--- models/keyword.go
+++ models/keyword.go
@@ -1,15 +1,17 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/beego/beego/v2/adapter/orm"
 )
 
 type Keyword struct {
 	Base
 
-	Name string
-	SearchCompleted bool `default:"false"`
-	User *User `orm:"rel(fk)"`
+	Name            string
+	SearchCompleted bool  `default:"false"`
+	User            *User `orm:"rel(fk)"`
 }
 
 func CreateKeyword(s *Keyword) (id int64, err error) {
@@ -31,7 +33,25 @@ func FindKeywordById(id int64) (keyword *Keyword, err error) {
 	return keyword, nil
 }
 
+func FetchKeywords(user *User) ([]*Keyword, error) {
+	if user == nil {
+		return nil, errors.New("User is blank")
+	}
+
+	var keywords []*Keyword
+
+	_, err := userKeywordsQuerySeter(user).OrderBy("-created_at").All(&keywords)
+
+	return keywords, err
+}
+
 // Beego by default creates the table name as singular, it will make it plural
 func (u *Keyword) TableName() string {
 	return "keywords"
+}
+
+func userKeywordsQuerySeter(user *User) orm.QuerySeter {
+	o := orm.NewOrm()
+
+	return o.QueryTable(Keyword{}).Filter("user_id", 9)
 }
