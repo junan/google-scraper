2b79fb853c300117115938828c7927a1f2ef71b2
Junan Chakma <junan@nimblehq.co>
2021-03-12 15:56:57 +0700

    Added valid spec

--- forms/search_form_test.go
+++ forms/search_form_test.go
@@ -6,6 +6,7 @@ import (
 	. "google-scraper/tests/fabricators"
 	. "google-scraper/tests/testing_helpers"
 
+	"github.com/beego/beego/v2/core/validation"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -98,6 +99,138 @@ var _ = Describe("SearchForm", func() {
 					err = forms.PerformSearch(file, header, &user)
 
 					Expect(err.Error()).To(Equal("File size can't be more than 5 MB."))
+				})
+			})
+		})
+	})
+
+	Describe("#Valid", func() {
+		Context("given the search attributes are valid", func() {
+			It("does NOT add error to validation", func() {
+				validCsvFilePath := AppRootDir(0) + "/fixtures/shared/valid_keywords.csv"
+				file, header, err := GetFormFileData(validCsvFilePath)
+				if err != nil {
+					Fail("Getting form file data failed: " + err.Error())
+				}
+
+				mockResponseFilePath := AppRootDir(0) + "/fixtures/services/crawler/valid_get_response.html"
+				MockCrawling(mockResponseFilePath)
+
+				csv := forms.CSV{
+					File:   file,
+					Header: header,
+					Size:   2,
+				}
+
+				formValidation := validation.Validation{}
+				csv.Valid(&formValidation)
+
+				Expect(len(formValidation.Errors)).To(BeZero())
+			})
+		})
+
+		Context("given the search attributes are INVALID", func() {
+			Context("given NO file", func() {
+				It("adds an error to validation", func() {
+					csv := forms.CSV{
+						File:   nil,
+						Header: nil,
+						Size:   0,
+					}
+
+					formValidation := validation.Validation{}
+					csv.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Message).To(Equal("File can't be blank."))
+				})
+			})
+
+			Context("given the file keywords are empty", func() {
+				It("adds an error to validation", func() {
+					filePath := AppRootDir(0) + "/fixtures/shared/empty_keyword.csv"
+					file, header, err := GetFormFileData(filePath)
+					if err != nil {
+						Fail("Getting form file data failed: " + err.Error())
+					}
+
+					csv := forms.CSV{
+						File:   file,
+						Header: header,
+						Size:   0,
+					}
+
+					formValidation := validation.Validation{}
+					csv.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Message).To(Equal("Keywords count can't be more than 1000 or less than 1."))
+				})
+			})
+
+			Context("given the CSV file is wrongly formatted", func() {
+				It("adds an error to validation", func() {
+					filePath := AppRootDir(0) + "/fixtures/shared/invalid_keyword.csv"
+					file, header, err := GetFormFileData(filePath)
+					if err != nil {
+						Fail("Getting form file data failed: " + err.Error())
+					}
+
+					csv := forms.CSV{
+						File:   file,
+						Header: header,
+						Size:   0,
+					}
+
+					formValidation := validation.Validation{}
+					csv.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Message).To(Equal("CSV contents are not in correct format."))
+				})
+			})
+
+			Context("given the file is of an INVALID type", func() {
+				It("adds an error to validation", func() {
+					filePath := AppRootDir(0) + "/fixtures/shared/test.jpeg"
+					file, header, err := GetFormFileData(filePath)
+					if err != nil {
+						Fail("Getting form file data failed: " + err.Error())
+					}
+
+					csv := forms.CSV{
+						File:   file,
+						Header: header,
+						Size:   0,
+					}
+
+					formValidation := validation.Validation{}
+					csv.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Message).To(Equal("Please upload the file in CSV format."))
+				})
+			})
+
+			Context("given the file size is more than 5 megabytes", func() {
+				It("adds an error to validation", func() {
+					filePath := AppRootDir(0) + "/fixtures/shared/big_file.pdf"
+					file, header, err := GetFormFileData(filePath)
+					if err != nil {
+						Fail("Getting form file data failed: " + err.Error())
+					}
+
+					csv := forms.CSV{
+						File:   file,
+						Header: header,
+						Size:   0,
+					}
+
+					formValidation := validation.Validation{}
+					csv.Valid(&formValidation)
+
+					Expect(len(formValidation.Errors)).To(Equal(1))
+					Expect(formValidation.Errors[0].Message).To(Equal("Please upload the file in CSV format."))
 				})
 			})
 		})
