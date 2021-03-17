dc2c9b1550db57f522a80db9a78bf210e5cb3195
Junan Chakma <junan@nimblehq.co>
2021-03-17 11:20:13 +0700

    Added user test

--- models/user_test.go
+++ models/user_test.go
@@ -1,6 +1,8 @@
 package models_test
 
 import (
+	"fmt"
+
 	_ "google-scraper/initializers"
 	"google-scraper/models"
 	. "google-scraper/tests"
@@ -10,6 +12,56 @@ import (
 )
 
 var _ = Describe("User", func() {
+	Describe("#Keywords", func() {
+		It("returns user keywords", func() {
+			user := FabricateUser("John", "john@example.com", "secret")
+			FabricateKeyword("Buy domain", false, &user)
+			FabricateKeyword("Buy bike", false, &user)
+			user2 := FabricateUser("David", "david@example.com", "secret")
+			FabricateKeyword("Buy car", false, &user2)
+
+			Expect(user.Keywords().Count()).To(BeNumerically("==", 2))
+		})
+	})
+
+	Describe("#PaginatedKeywords", func() {
+		Context("given the paginatesPer is 10", func() {
+			It("returns 10 keywords", func() {
+				user := FabricateUser("John", "john@example.com", "secret")
+
+				// Creating 11 keywords record
+				for i := 1; i <= 11; i++ {
+					FabricateKeyword(fmt.Sprintf("Buy domain %d", i), false, &user)
+				}
+
+				paginatedKeywords, err := user.PaginatedKeywords(user.Keywords(), 0, 10)
+				if err != nil {
+					Fail("Getting paginated keywords failed: " + err.Error())
+				}
+
+				Expect(len(paginatedKeywords)).To(BeNumerically("==", 10))
+			})
+		})
+
+		Context("given the paginatesPer is 5", func() {
+			It("returns 5 keywords", func() {
+				user := FabricateUser("John", "john@example.com", "secret")
+
+				// Creating 6 keywords record
+				for i := 1; i <= 6; i++ {
+					FabricateKeyword(fmt.Sprintf("Buy domain %d", i), false, &user)
+				}
+
+				paginatedKeywords, err := user.PaginatedKeywords(user.Keywords(), 0, 5)
+				if err != nil {
+					Fail("Getting paginated keywords failed: " + err.Error())
+				}
+
+				Expect(len(paginatedKeywords)).To(BeNumerically("==", 5))
+			})
+		})
+	})
+
 	Describe("#CreateUser", func() {
 		Context("given the user with valid params", func() {
 			It("returns the user ID", func() {
