4ec038a9b7a016107c355a3d8a77c25ff5dff166
Junan Chakma <junan@nimblehq.co>
2021-03-22 11:22:49 +0700

    Added render html test

--- controllers/keyword_test.go
+++ controllers/keyword_test.go
@@ -95,6 +95,89 @@ var _ = Describe("KeywordController", func() {
 		})
 	})
 
+	Describe("GET /keyword/:id/render-html", func() {
+		Context("given the user is an authenticated user", func() {
+			Context("given the keyword is belong to the user", func() {
+				It("returns 200 status code", func() {
+					user := FabricateUser("John", "john@example.com", "secret")
+					keyword := FabricateKeyword("Buy domain", false, &user)
+					url := fmt.Sprintf("/keyword/%d/render-html", keyword.Id)
+
+					response := MakeAuthenticatedRequest("GET", url, nil, &user)
+
+					Expect(response.StatusCode).To(Equal(http.StatusOK))
+				})
+			})
+
+			Context("given the keyword is NOT belong to the user", func() {
+				It("redirects to the root path", func() {
+					user1 := FabricateUser("John", "john@example.com", "secret")
+					FabricateKeyword("Buy domain", false, &user1)
+					user2 := FabricateUser("Mike", "mike@example.com", "secret")
+					keyword2 := FabricateKeyword("Buy bike", false, &user2)
+					url := fmt.Sprintf("/keyword/%d/render-html", keyword2.Id)
+
+					response := MakeAuthenticatedRequest("GET", url, nil, &user1)
+					currentPath := GetUrlPath(response)
+
+					Expect(response.StatusCode).To(Equal(http.StatusFound))
+					Expect(currentPath).To(Equal("/"))
+				})
+
+				It("sets the flash error message", func() {
+					user1 := FabricateUser("John", "john@example.com", "secret")
+					FabricateKeyword("Buy domain", false, &user1)
+					user2 := FabricateUser("Mike", "mike@example.com", "secret")
+					keyword2 := FabricateKeyword("Buy bike", false, &user2)
+					url := fmt.Sprintf("/keyword/%d/render-html", keyword2.Id)
+
+					response := MakeAuthenticatedRequest("GET", url, nil, &user1)
+					flash := GetFlash(response.Cookies())
+
+					Expect(flash.Data["error"]).To(Equal("Keyword not found."))
+				})
+			})
+
+			Context("given the keyword does NOT exist in the database", func() {
+				It("redirects to the root path", func() {
+					user := FabricateUser("John", "john@example.com", "secret")
+					FabricateKeyword("Buy domain", false, &user)
+					url := fmt.Sprintf("/keyword/%d/render-html", 1000)
+
+					response := MakeAuthenticatedRequest("GET", url, nil, &user)
+					currentPath := GetUrlPath(response)
+
+					Expect(response.StatusCode).To(Equal(http.StatusFound))
+					Expect(currentPath).To(Equal("/"))
+				})
+
+				It("sets the flash error message", func() {
+					user := FabricateUser("John", "john@example.com", "secret")
+					FabricateKeyword("Buy domain", false, &user)
+					url := fmt.Sprintf("/keyword/%d/render-html", 1000)
+
+					response := MakeAuthenticatedRequest("GET", url, nil, &user)
+
+					flash := GetFlash(response.Cookies())
+
+					Expect(flash.Data["error"]).To(Equal("Keyword not found."))
+				})
+			})
+		})
+
+		Context("given the user is a guest user", func() {
+			It("redirects to the login page", func() {
+				url := fmt.Sprintf("/keyword/%d", 1)
+
+				response := MakeRequest("GET", url, nil)
+				currentPath := GetUrlPath(response)
+
+				Expect(response.StatusCode).To(Equal(http.StatusFound))
+				Expect(currentPath).To(Equal("/login"))
+			})
+		})
+	})
+
 	AfterEach(func() {
 		TruncateTables("users", "keywords")
 	})
