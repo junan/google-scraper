cf4c0052b1811bf1841f6be0762c35e7023f67d0
Junan Chakma <junan@nimblehq.co>
2021-03-29 10:37:06 +0700

    Add base controller

---
+++ controllers/api/v1/base.go
@@ -0,0 +1,32 @@
+package apiv1
+
+import (
+	"net/http"
+
+	"github.com/beego/beego/v2/server/web"
+	"github.com/google/jsonapi"
+)
+
+type baseAPIController struct {
+	web.Controller
+}
+
+func (c *baseAPIController) serveJSON(data interface{}) {
+	response, err := jsonapi.Marshal(data)
+	if err != nil {
+		c.renderError(err)
+		return
+	}
+
+	c.Data["json"] = response
+	c.ServeJSON()
+}
+
+func (c *baseAPIController) renderError(err error){
+	err = jsonapi.MarshalErrors(c.Ctx.ResponseWriter, []*jsonapi.ErrorObject{{
+		Detail: err.Error(),
+	}})
+	if err != nil {
+		http.Error(c.Ctx.ResponseWriter, err.Error(), http.StatusInternalServerError)
+	}
+}
