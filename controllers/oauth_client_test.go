8ff6ab44eaa3db116b8f61a36d2797debfec6a16
Junan Chakma <junan@nimblehq.co>
2021-03-30 12:50:45 +0700

    Create action test

--- controllers/oauth_client_test.go
+++ controllers/oauth_client_test.go
@@ -2,6 +2,7 @@ package controllers_test
 
 import (
 	"net/http"
+	"strings"
 
 	_ "google-scraper/initializers"
 	. "google-scraper/tests"
@@ -25,6 +26,42 @@ var _ = Describe("OauthClientController", func() {
 		Context("given the user is a guest user", func() {
 			It("redirects to the login page", func() {
 				response := MakeRequest("GET", "/oauth-client", nil)
+				currentPath := GetUrlPath(response)
+
+				Expect(response.StatusCode).To(Equal(http.StatusFound))
+				Expect(currentPath).To(Equal("/login"))
+			})
+		})
+	})
+
+	Describe("POST /oauth-client", func() {
+		Context("given the user is an authenticated user", func() {
+			It("sets the success flash message", func() {
+				user := FabricateUser("John", "john@example.com", "secret")
+
+				body := strings.NewReader("")
+				response := MakeAuthenticatedRequest("POST", "/oauth-client", body, &user)
+				flash := GetFlash(response.Cookies())
+
+				Expect(flash.Data["success"]).To(Equal("Your oauth client has been created successfully"))
+			})
+
+			It("redirects to the oauth client page", func() {
+				user := FabricateUser("John", "john@example.com", "secret")
+				body := strings.NewReader("")
+
+				response := MakeAuthenticatedRequest("POST", "/oauth-client", body, &user)
+				path := GetUrlPath(response)
+
+				Expect(response.StatusCode).To(Equal(http.StatusFound))
+				Expect(path).To(Equal("/oauth-client"))
+			})
+		})
+
+		Context("given the user is a guest user", func() {
+			It("redirects to the login page", func() {
+				body := strings.NewReader("")
+				response := MakeRequest("POST", "/oauth-client", body)
 				currentPath := GetUrlPath(response)
 
 				Expect(response.StatusCode).To(Equal(http.StatusFound))
