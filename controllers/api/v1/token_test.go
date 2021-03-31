1621d52ff006aa0131ea7912fe454d48ef6283e6
Junan Chakma <junan@nimblehq.co>
2021-03-31 13:28:24 +0700

    Add token test

--- controllers/api/v1/token_test.go
+++ controllers/api/v1/token_test.go
@@ -3,39 +3,192 @@ package apiv1_test
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 
+	. "google-scraper/helpers"
+	. "google-scraper/serializers"
 	. "google-scraper/tests"
 
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gstruct"
+	"github.com/tidwall/gjson"
 )
 
 var _ = Describe("TokenController", func() {
-	Describe("GET /api/v1/token", func() {
-		It("returns 200 status code", func() {
-			response := MakeRequest("GET", "/api/v1/token", nil)
+	Describe("POST /api/v1/token", func() {
+		Context("Given the valid credential", func() {
+			It("returns 200 status code", func() {
+				email := "john@example.com"
+				password := "secret"
+				client := FabricateOAuthClient(uuid.New().String(), uuid.New().String())
+				user := FabricateUser("John", email, password)
 
-			Expect(response.StatusCode).To(Equal(http.StatusOK))
+				form := url.Values{
+					"client_id":     {client.ID},
+					"client_secret": {client.Secret},
+					"grant_type":    {"password"},
+					"username":      {email},
+					"password":      {password},
+				}
+				body := strings.NewReader(form.Encode())
+
+				response := MakeAuthenticatedRequest("POST", "/api/v1/token", body, &user)
+
+				Expect(response.StatusCode).To(Equal(http.StatusOK))
+			})
+
+			It("returns current json response", func() {
+				email := "john@example.com"
+				password := "secret"
+				client := FabricateOAuthClient(uuid.New().String(), uuid.New().String())
+				user := FabricateUser("John", email, password)
+				form := url.Values{
+					"client_id":     {client.ID},
+					"client_secret": {client.Secret},
+					"grant_type":    {"password"},
+					"username":      {email},
+					"password":      {password},
+				}
+				body := strings.NewReader(form.Encode())
+				response := MakeAuthenticatedRequest("POST", "/api/v1/token", body, &user)
+				responseBody, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					Fail("Reading response body failed: " + err.Error())
+				}
+
+				id, err := StringToInt(gjson.Get(string(responseBody), "data.id").String())
+				if err != nil {
+					Fail("Converting failed: " + err.Error())
+				}
+
+				tokenResponse := TokenResponse{
+					Id:           id,
+					AccessToken:  gjson.Get(string(responseBody), "data.attributes.access_token").String(),
+					ExpiresIn:    gjson.Get(string(responseBody), "data.attributes.expires_in").Int(),
+					RefreshToken: gjson.Get(string(responseBody), "data.attributes.refresh_token").String(),
+					TokenType:    gjson.Get(string(responseBody), "data.attributes.token_type").String(),
+				}
+
+				Expect(tokenResponse).To(gstruct.MatchAllFields(gstruct.Fields{
+					"Id":           BeNumerically("==", 0),
+					"AccessToken":  Not(BeEmpty()),
+					"ExpiresIn":    BeNumerically("==", 7200),
+					"RefreshToken": Not(BeEmpty()),
+					"TokenType":    Equal("Bearer"),
+				}))
+
+			})
 		})
 
-		It("returns correct JSON response", func() {
-			expectedResponse := `{
-				"data": {
-					"type": "health_check",
-					"id": "0",
-					"attributes": {
-						"success": true
+		Context("Given the INVALID credential", func() {
+			Context("Given the user credential is INVALID", func() {
+				It("returns 401 unauthorized status code", func() {
+					email := "john@example.com"
+					password := "secret"
+					client := FabricateOAuthClient(uuid.New().String(), uuid.New().String())
+					user := FabricateUser("John", email, password)
+
+					form := url.Values{
+						"client_id":     {client.ID},
+						"client_secret": {client.Secret},
+						"grant_type":    {"password"},
+						"username":      {email},
+						"password":      {"invalid"},
 					}
-				}
-			}`
+					body := strings.NewReader(form.Encode())
 
-			response := MakeRequest("GET", "/api/v1/health-check", nil)
-			responseBody, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				Fail("Reading response body failed: " + err.Error())
-			}
+					response := MakeAuthenticatedRequest("POST", "/api/v1/token", body, &user)
 
-			Expect(string(responseBody)).To(MatchJSON(expectedResponse))
+					Expect(response.StatusCode).To(Equal(http.StatusUnauthorized))
+				})
+
+				It("returns correct json response", func() {
+					email := "john@example.com"
+					password := "secret"
+					client := FabricateOAuthClient(uuid.New().String(), uuid.New().String())
+					user := FabricateUser("John", email, password)
+					expectedResponse := `{
+						"errors": [
+							{
+								"detail": "Client authentication failed"
+							}
+						]
+					}`
+
+					form := url.Values{
+						"client_id":     {client.ID},
+						"client_secret": {client.Secret},
+						"grant_type":    {"password"},
+						"username":      {email},
+						"password":      {"invalid"},
+					}
+					body := strings.NewReader(form.Encode())
+
+					response := MakeAuthenticatedRequest("POST", "/api/v1/token", body, &user)
+					responseBody, err := ioutil.ReadAll(response.Body)
+					if err != nil {
+						Fail("Reading response body failed: " + err.Error())
+					}
+
+					Expect(string(responseBody)).To(MatchJSON(expectedResponse))
+				})
+			})
+
+			Context("Given the oauth client credential is INVALID", func() {
+				It("returns 401 unauthorized status code", func() {
+					email := "john@example.com"
+					password := "secret"
+					client := FabricateOAuthClient(uuid.New().String(), uuid.New().String())
+					user := FabricateUser("John", email, password)
+
+					form := url.Values{
+						"client_id":     {client.ID},
+						"client_secret": {"invalid"},
+						"grant_type":    {"password"},
+						"username":      {email},
+						"password":      {password},
+					}
+					body := strings.NewReader(form.Encode())
+
+					response := MakeAuthenticatedRequest("POST", "/api/v1/token", body, &user)
+
+					Expect(response.StatusCode).To(Equal(http.StatusUnauthorized))
+				})
+
+				It("returns correct json response", func() {
+					email := "john@example.com"
+					password := "secret"
+					client := FabricateOAuthClient(uuid.New().String(), uuid.New().String())
+					user := FabricateUser("John", email, password)
+					expectedResponse := `{
+						"errors": [
+							{
+								"detail": "Client authentication failed"
+							}
+						]
+					}`
+
+					form := url.Values{
+						"client_id":     {client.ID},
+						"client_secret": {"invalid"},
+						"grant_type":    {"password"},
+						"username":      {email},
+						"password":      {"invalid"},
+					}
+					body := strings.NewReader(form.Encode())
+
+					response := MakeAuthenticatedRequest("POST", "/api/v1/token", body, &user)
+					responseBody, err := ioutil.ReadAll(response.Body)
+					if err != nil {
+						Fail("Reading response body failed: " + err.Error())
+					}
+
+					Expect(string(responseBody)).To(MatchJSON(expectedResponse))
+				})
+			})
 		})
 	})
 })
--- serializers/token.go
+++ serializers/token.go
@@ -1,7 +1,7 @@
 package serializers
 
 type TokenResponse struct {
-	Id           int    `jsonapi:"primary,token"`
+	Id           int64    `jsonapi:"primary,token"`
 	AccessToken  string `jsonapi:"attr,access_token"`
 	ExpiresIn    int64  `jsonapi:"attr,expires_in"`
 	RefreshToken string `jsonapi:"attr,refresh_token"`
