7cd3cbff93be21a07ab4790de94d745d78c6a185
Junan Chakma <junan@nimblehq.co>
2021-04-02 15:51:55 +0700

    Removed unused code

--- controllers/api/v1/keyword.go
+++ controllers/api/v1/keyword.go
@@ -3,12 +3,9 @@ package apiv1
 import (
 	"net/http"
 
-	"google-scraper/forms"
-	. "google-scraper/helpers"
-	"google-scraper/models"
-	"google-scraper/services/oauth"
-
 	"github.com/beego/beego/v2/core/logs"
+
+	"google-scraper/forms"
 )
 
 type Keyword struct {
@@ -16,28 +13,11 @@ type Keyword struct {
 }
 
 func (c *Keyword) Upload() {
-	tokenInfo, err := oauth.OauthServer.ValidationBearerToken(c.Ctx.Request)
-	if err != nil {
-		c.renderError(err, http.StatusInternalServerError)
-		return
-	}
-
-	userID, err := StringToInt(tokenInfo.GetUserID())
-	if err != nil {
-		c.renderError(err, http.StatusInternalServerError)
-		return
-	}
-
-	user, err := models.FindUserById(userID)
-	if err != nil {
-		c.renderError(err, http.StatusNotFound)
-	}
-
 	file, header, err := c.GetFile("file")
 	if err != nil {
 		logs.Error("Getting file failed: ", err)
 	}
-	err = forms.PerformSearch(file, header, user)
+	err = forms.PerformSearch(file, header, c.CurrentUser)
 	if err != nil {
 		c.renderError(err, http.StatusInternalServerError)
 	}
